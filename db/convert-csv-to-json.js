bb5cef237cdcb19c458035bb716e00c281673ed0
hayes28 <heather.hayes918@gmail.com>
2023-12-15 11:55:42 -0600

    Fixed the convert data file, the data output file is formatted correctly now

--- db/convert-csv-to-json.js
+++ db/convert-csv-to-json.js
@@ -2,15 +2,40 @@ const csv = require("csvtojson");
 const fs = require("fs");
 const path = require("path");
 
-const csvFilePath = path.join(__dirname, "HLG_data - Sheet1.csv");
-const jsonFilePath = path.join(
+const csvFilePath = path.join(
   __dirname,
-  "data.json"
+  "HLG_data - Sheet1 (4).csv"
 );
+const jsonFilePath = path.join(__dirname, "data.json");
 
 csv()
   .fromFile(csvFilePath)
   .then((jsonObj) => {
-    console.log(jsonObj); // log the JSON array
-    fs.writeFileSync(jsonFilePath, JSON.stringify(jsonObj, null, 2), "utf-8"); // write to the json file
+    // Convert color and hex strings into arrays and remove all newline characters
+    const updatedJsonObj = jsonObj.map((painting) => {
+      return {
+        ...painting,
+        // Remove all types of newline characters, then split by comma and trim each entry
+        colors: painting.colors
+          .replace(/(\r\n|\n|\r)/gm, "")
+          .split(",")
+          .map((color) => color.trim()),
+        color_hex: painting.color_hex
+          .replace(/(\r\n|\n|\r)/gm, "")
+          .split(",")
+          .map((hex) => hex.trim()),
+        subjects: painting.subjects
+          .replace(/(\r\n|\n|\r)/gm, "")
+          .split(",")
+          .map((subject) => subject.trim()),
+        painting_url: painting.painting_url.replace(/(\r\n|\n|\r)/gm, ""),
+      };
+    });
+
+    console.log(updatedJsonObj); // log the updated JSON array
+    fs.writeFileSync(
+      jsonFilePath,
+      JSON.stringify(updatedJsonObj, null, 2),
+      "utf-8"
+    ); // write to the JSON file
   });
